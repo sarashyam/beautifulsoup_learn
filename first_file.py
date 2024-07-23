d4cb2ed5f38320f81a34a01a1f1ff925cead24a6
saradha <saradhashyam0401@gmail.com>
2024-07-23 09:41:04 +0530

    first commit

---
+++ first_file.py
@@ -0,0 +1,32 @@
+# import requests
+from bs4 import BeautifulSoup
+
+# url = 'https://en.wikipedia.org/wiki/HTML'
+
+# r = requests.get(url)
+# print(r.text)
+# with open('sample.html','w') as f:
+#     f.write(r.text)
+
+import requests
+
+# url = 'https://en.wikipedia.org/wiki/HTML'
+
+# # Make the HTTP request
+# r = requests.get(url)
+
+# # Check if the request was successful
+# if r.status_code == 200:
+#     # Open the file in write mode and ensure the correct encoding
+#     with open('sample.html', 'w', encoding='utf-8') as f:
+#         f.write(r.text)
+# else:
+#     print(f"Failed to retrieve the page. Status code: {r.status_code}")
+
+with open('sample.html','r', encoding='utf-8') as f:
+    html_doc = f.read()
+soup = BeautifulSoup(html_doc,'html.parser')
+# print(soup.prettify())
+print(soup.title) # <title>HTML - Wikipedia</title>
+print(soup.title.name) # title
+print(soup.title.string) # HTML - Wikipedia
